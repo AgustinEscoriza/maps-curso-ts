424e3e2edfd045cb3a63c03b8f8f1c381297b537
AgusEscoriza <AgusEscoriza@outlook.es>
2023-09-25 09:13:58 -0300

    curso terminado

--- src/User.ts
+++ src/User.ts
@@ -1,17 +1,24 @@
 import { faker } from "@faker-js/faker";
 
-export class User {
+import { Mappable } from "./CustomMap";
+
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = "red";
 
   constructor() {
-    this.name = faker.person.firstName();
+    this.name = faker.name.firstName();
     this.location = {
-      lat: faker.location.latitude(),
-      lng: faker.location.longitude()
-    }
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
   }
-};
+
+  markerContent(): string {
+    return `User Name: ${this.name}`;
+  }
+}
